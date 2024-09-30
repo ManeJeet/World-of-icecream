1ce41aec1d83c94d7d2c338c0adb2cb3f1af93f1
Jeet Manoj Mane <jeetmane3801@gmail.com>
2024-09-30 23:34:36 +1000

    added ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,11 +3,18 @@ module.exports = {
     apps: [
       {
         name: "World-of-icecream",
-        script: "./index.js", // Adjust the script path as needed
+        script: "./server.js", // Replace with your main application file
         instances: "max", // Or specify a number like 2
         exec_mode: "cluster", // Or "fork" for single instance
         env: {
-          NODE_ENV: "production"
+          NODE_ENV: "development",
+          PORT: 3000
+          // Add other development-specific variables here
+        },
+        env_production: {
+          NODE_ENV: "production",
+          PORT: 8080
+          // Add other production-specific variables here
         }
       }
     ]
